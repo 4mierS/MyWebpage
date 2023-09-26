9b640035618d2233984c612f3065d455a636da02
Amier Shraideh <amiershr@gmail.com>
2023-09-26 21:27:13 +0200

    update overlay
    overlay gets removed and if i open, one new overlay get created

--- functions.js
+++ functions.js
@@ -1,4 +1,14 @@
-var overlays = []; // Declare an array to store created overlays
+// Description: This file contains all the functions that are used in the website
+var overlay = document.createElement('div');
+overlay.id = "overlay";
+overlay.style.position = "fixed";
+overlay.style.top = "0";
+overlay.style.left = "0";
+overlay.style.width = "100%";
+overlay.style.height = "100%";
+overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+overlay.style.zIndex = "8";
+
 
 function opened() {
   console.log("opened() was called");
@@ -10,6 +20,9 @@ function opened() {
       var popupBtn = document.getElementById(e.target.id);
       var popupStr = e.target.parentNode.parentNode.id;
       console.log("This is: " + popupStr);
+        
+      //if the popup is open, i cant srcoll
+        document.body.style.overflow = "hidden";
 
       popup.style.display = "block";
       popup.style.position = "absolute";
@@ -23,35 +36,15 @@ function opened() {
       popup.style.borderRadius = "10px";
       popup.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)";
       popupBtn.style.display = "none";
-
-      // Create an overlay div so that you can't click on the background
-      var overlay = document.createElement('div');
-      overlay.id = "overlay";
-      overlay.style.position = "fixed";
-      overlay.style.top = "0";
-      overlay.style.left = "0";
-      overlay.style.width = "100%";
-      overlay.style.height = "100%";
-      overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
-      overlay.style.zIndex = "8";
       document.body.appendChild(overlay);
-
-      // Push the overlay into the array
-      overlays.push(overlay);
 
       popup.onclick = function () {
         popup.style = {};
         popupBtn.style.display = "block";
-
-        // Remove all overlays
-        overlays.forEach(function (overlay) {
-          if (overlay.parentNode) {
-            document.body.removeChild(overlay);
-          }
-        });
-
-        // Clear the overlays array
-        overlays = [];
+        
+        if (document.body.contains(overlay)) {
+          document.body.removeChild(overlay);
+        }
       }
     }
   });
