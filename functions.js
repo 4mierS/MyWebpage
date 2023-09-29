4265be1f6f3e849a8e948e3b9ec0c6a0d6a3cde6
Amier Shraideh <amiershr@gmail.com>
2023-09-29 19:03:33 +0200

    Scrollbug fixed
    Added logic to disable scrolling if popup view is open

--- functions.js
+++ functions.js
@@ -22,7 +22,37 @@ function opened() {
       console.log("This is: " + popupStr);
         
       //if the popup is open, i cant srcoll
-        document.body.style.overflow = "hidden";
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable Scrolling
+      window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+      window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+      window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+      window.addEventListener('keydown', preventDefaultForScrollKeys, false);
 
       popup.style.display = "block";
       popup.style.position = "absolute";
@@ -41,6 +71,12 @@ function opened() {
       popup.onclick = function () {
         popup.style = {};
         popupBtn.style.display = "block";
+
+      //Call this to Enable Scrolling 
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+        window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+        window.removeEventListener('touchmove', preventDefault, wheelOpt);
+        window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
         
         if (document.body.contains(overlay)) {
           document.body.removeChild(overlay);
